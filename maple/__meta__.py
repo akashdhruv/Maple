37c06beaf140ecce71cbd9350b22843cdbc263f1
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-09-01 17:07:46 -0500

    update pkg version

--- maple/__meta__.py
+++ maple/__meta__.py
@@ -1,7 +1,7 @@
 """Metadata for MapleUtil package"""
 
 __pkgname__ = "MapleUtil"
-__version__ = "0.2"
+__version__ = "0.3"
 __authors__ = "Akash Dhruv"
 __license__ = "MIT License"
 __copyright__ = "Copyright (c) Akash Dhruv 2021. All Rights Reserved."
